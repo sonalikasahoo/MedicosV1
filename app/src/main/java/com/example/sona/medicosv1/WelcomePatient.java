78762d5898b6c750d98dffd4a649e6abd1d15aab
sonalika <sonalikasahoo6@gmail.com>
2018-03-19 18:23:46 +0530

    String resources added to first five activities

---
+++ app/src/main/java/com/example/sona/medicosv1/WelcomePatient.java
@@ -0,0 +1,13 @@
+package com.example.sona.medicosv1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WelcomePatient extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome_patient);
+    }
+}
